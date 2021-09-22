e7c75b99c034b2681dfbb26c0f9b9fd0c550987e
wuyd <wuydit@163.com>
2021-09-22 14:15:08 +0800

    feat(eureka): eureka地址修改,jar依赖修改
    Signed-off-by: wuyd <wuydit@163.com>

--- lair-provider-payment-9002/src/main/java/com/bigbear/payment/LairProviderPayment9002Application.java
+++ lair-provider-payment-9002/src/main/java/com/bigbear/payment/LairProviderPayment9002Application.java
@@ -2,10 +2,12 @@ package com.bigbear.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wuyd
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class LairProviderPayment9002Application {
 
