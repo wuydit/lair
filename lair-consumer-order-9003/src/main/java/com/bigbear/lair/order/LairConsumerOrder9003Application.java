2b47bee44e1f52517311a8d30e4b2191f5f35ae8
wuyd <wuydit@163.com>
2021-09-22 14:25:53 +0800

    feat(eureka): eureka client 带入
    Signed-off-by: wuyd <wuydit@163.com>

--- lair-consumer-order-9003/src/main/java/com/bigbear/lair/order/LairConsumerOrder9003Application.java
+++ lair-consumer-order-9003/src/main/java/com/bigbear/lair/order/LairConsumerOrder9003Application.java
@@ -2,10 +2,12 @@ package com.bigbear.lair.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wuyd
  */
+@EnableEurekaClient
 @SpringBootApplication
 public class LairConsumerOrder9003Application {
 
