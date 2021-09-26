7295c7491d34f566fc628192fbde4e0bb97e8602
wuyd <wuydit@163.com>
2021-09-26 19:27:31 +0800

    feat(config): 配置文件
    Signed-off-by: wuyd <wuydit@163.com>

--- lair-config-server-9011/src/main/java/com/bigbear/config/LairConfigServer9011Application.java
+++ lair-config-server-9011/src/main/java/com/bigbear/config/LairConfigServer9011Application.java
@@ -3,11 +3,14 @@ package com.bigbear.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wuyd
  */
+@EnableDiscoveryClient
+@EnableConfigServer
 @EnableEurekaClient
 @SpringBootApplication
 public class LairConfigServer9011Application {
