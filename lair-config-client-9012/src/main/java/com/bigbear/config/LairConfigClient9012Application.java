c3426048aa637a841204d1389248d72927238d02
wuyd <wuydit@163.com>
2021-09-27 10:05:09 +0800

    feat(config): config
    Signed-off-by: wuyd <wuydit@163.com>

---
+++ lair-config-client-9012/src/main/java/com/bigbear/config/LairConfigClient9012Application.java
@@ -0,0 +1,19 @@
+package com.bigbear.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wuyd
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class LairConfigClient9012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LairConfigClient9012Application.class, args);
+	}
+
+}
---
+++ lair-config-client-9012/src/test/java/com/bigbear/config/LairConfigClient9012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigbear.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LairConfigClient9012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
